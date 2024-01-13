e92cdc46135779bc3ab0e0e447253ac5f2593598
rona-eed <rona.eed@silverfort.com>
2024-01-13 20:56:48 +0200

    setup

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -2,29 +2,35 @@ function calculatePercentageOfTotal() {
     var percent = document.getElementById('percentOfTotal').value;
     var total = document.getElementById('totalForPercentage').value;
     var result = (percent / 100) * total;
-    document.getElementById('resultPercentageOfTotal').innerText = `Result: ${result}`;
+    document.getElementById('resultPercentageOfTotal').innerText = `Result: ${formatNumber(result)}`;
 }
 
 function calculateAmountAsPercentage() {
     var amount = document.getElementById('amount').value;
     var total = document.getElementById('totalAmount').value;
     var result = (amount / total) * 100;
-    document.getElementById('resultAmountAsPercentage').innerText = `Result: ${result.toFixed(2)}%`;
+    document.getElementById('resultAmountAsPercentage').innerText = `Result: ${formatNumber(result.toFixed(2))}%`;
 }
 
 function calculateTotalFromPercentage() {
     var partialAmount = document.getElementById('partialAmount').value;
     var percentage = document.getElementById('percentageOfTotal').value;
     var result = (partialAmount / percentage) * 100;
-    document.getElementById('resultTotalFromPercentage').innerText = `Result: ${result}`;
+    document.getElementById('resultTotalFromPercentage').innerText = `Result: ${formatNumber(result)}`;
 }
 
-// Optional: Add event listeners for 'Enter' key in input fields
+function formatNumber(number) {
+    return number.toLocaleString();
+}
 document.querySelectorAll('input[type="number"]').forEach(item => {
     item.addEventListener('keypress', function (e) {
         if (e.key === 'Enter') {
             e.preventDefault();
-            item.nextElementSibling.click();
+            let parentDiv = item.closest('div');
+            let button = parentDiv.querySelector('button');
+            if (button) {
+                button.click();
+            }
         }
     });
 });
